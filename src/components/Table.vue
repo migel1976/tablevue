58601f664c4e0eca0050bf2b6d8f3fb3a5ffee43
mike smirnov <migel1976@gmail.com>
2021-06-16 09:40:50 +0300

    обновил проект, теперь происходит корретное crud операции

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,47 +1,46 @@
 <template>
     <div>
-       <DxDataGrid
-         id='grid'
-         :show-borders='true'
-         :data-source='ordersData'
-         :repaint-changes-only='true'
-        >
-         <DxEditing
-            :refresh-mode='refresh-mode'
-            :allow-adding='true'
-            :allow-updating='true'
-            :allow-deleting='true'
-            mode='cell'
-         />
-         <DxColumn
-            data-field='CustomerID'
-            caption='Customer'
-         >
-           <DxLookup
-              :data-source='customersData'
-              value-expr='Value'
-              display-expr='Text'
-           />
-         </DxColumn>         
-         <DxColumn
-            data-field='OrderDate'
-            data-type='date'
-         />
-         <DxColumn data-field='Freight' />
-         <DxColumn data-field='ShipCountry' />
-         <DxColumn 
-                   data-field='ShipVia'
-                   caption='Shipping Company'
-                   data-type='number'
-         >
-            <DxLookup
-                    :data-source='shippersData'
-                    value-expr='Value'
-                    display-expr='Text'
-            />
-        </DxColumn>
-        <DxScrolling mode='virtual' />
-      </DxDataGrid>
+      <DxDataGrid
+        id="grid"
+        :show-borders="true"
+        :data-source="ordersData"
+        :repaint-changes-only="true"
+      >
+      <DxEditing
+        :refresh-mode="refreshMode"
+        :allow-adding="true"
+        :allow-updating="true"
+        :allow-deleting="true"
+        mode="row"
+      />
+      <DxColumn
+        data-field="CustomerID"
+        caption="Customer"
+      >
+        <DxLookup
+          :data-source="customersData"
+          value-expr="Value"
+          display-expr="Text"
+        />
+      </DxColumn>
+      <DxColumn
+        data-field="OrderDate"
+        data-type="date"
+      />
+      <DxColumn data-field="Freight"/>
+      <DxColumn data-field="ShipCountry"/>
+      <DxColumn
+        data-field="ShipVia"
+        caption="Shipping Company"
+        data-type="number"
+      >
+        <DxLookup
+          :data-source="shippersData"
+          value-expr="Value"
+          display-expr="Text"
+        />
+      </DxColumn>
+    </DxDataGrid>
     </div>
 </template>
 
@@ -50,8 +49,10 @@
         DxDataGrid,
         DxColumn,
         DxEditing,
+        /* DxScrolling, */
+        /* DxSummary, */
         DxLookup,
-        DxScrolling
+        /* DxTotalItem */
     }
     from 'devextreme-vue/data-grid';
     import CustomStore from 'devextreme/data/custom_store';
@@ -71,76 +72,83 @@
             DxDataGrid,
             DxColumn,
             DxEditing,
+            /* DxScrolling, */
+            /* DxSummary, */
             DxLookup,
-            DxScrolling
+            /* DxTotalItem, */
         },
-        data(){
-            return{
-                ordersData:new CustomStore({
-                    key:'OrderID',
-                    load:()=>this.sendRequest(`${URL}/Orders`),
-                    insert:(values)=>this.sendRequest(`{URL}/InsertOrder`,'POST',{
-                        values:JSON.stringify(values)
-                    }),
-                    update:(key,values)=>this.sendRequest(`${URL}/UpdateOrder`,'PUT',{
-                        key:key,
-                        values:JSON.stringify(values)
-                    }),
-                    remove:(key)=>this.sendRequest(`${URL}/DeleteOrder`,'DELETE',{
-                        key:key
-                    })
-                }),
-                customersData:new CustomStore({
-                    key:'Value',
-                    loadMode:'raw',
-                    load:()=>this.sendRequest(`${URL}/CustomersLookup`)
-                }),
-                shippersData:new CustomStore({
-                    key:'Value',
-                    loadMode:'raw',
-                    load:()=>this.sendRequest(`${URL}/ShippersLookup`)
-                }),
-                request:[],
-                refreshMode:'reshape'
-            }
+       data() {
+          return {
+            ordersData: new CustomStore({
+              key: 'OrderID',
+              load: () => this.sendRequest(`${URL}/Orders`),
+              insert: (values) => this.sendRequest(`${URL}/InsertOrder`, 'POST', {
+                values: JSON.stringify(values)
+              }),
+              update: (key, values) => this.sendRequest(`${URL}/UpdateOrder`, 'PUT', {
+                key: key,
+                values: JSON.stringify(values)
+              }),
+              remove: (key) => this.sendRequest(`${URL}/DeleteOrder`, 'DELETE', {
+                key: key
+              })
+            }),
+            customersData: new CustomStore({
+              key: 'Value',
+              loadMode: 'raw',
+              load: () => this.sendRequest(`${URL}/CustomersLookup`)
+            }),
+            shippersData: new CustomStore({
+              key: 'Value',
+              loadMode: 'raw',
+              load: () => this.sendRequest(`${URL}/ShippersLookup`)
+            }),
+            requests: [],
+            refreshMode: 'reshape',
+            refreshModes: ['full', 'reshape', 'repaint']
+          };
         },
         methods:{
             initMessages() {
                 loadMessages(ruMessages);
             },
-            sendRequest(url, method, data){
-            /* sendRequest(url, method){ */
-                method=method || 'GET';
-                data=data||{};
-                const params=Object.keys(data).map((key)=>{
-                    return `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`;
-                }).join('&');
-                if(method==='GET'){
-                    return fetch(url,{
-                        method:method,
-                        credentials:'include'
-                        }).then(result=>result.json().then(json=>{
-                        if(result.ok) return json.data;
-                        throw json.Message;
-                        }));
-               }//end if==='GET'
-               return fetch(url,{
-                    method:method,
-                    body:params,
-                    headers:{
-                       'Content-Type':'application/x-www-form-urlencoded;charset=UTF-8'
-                    },
-                    credentials:'include'
-                    }).then(result=>{
-                    if(result.ok){
-                        return result.text().then(text=>text && JSON.parse(text));
-                    }else{
-                        return result.json().then(json=>{
-                            throw json.Message;
-                      });
-                    }
-                });
-            }//end sendRequest
+        sendRequest(url, method, data) {
+              method = method || 'GET';
+              data = data || {};
+
+              /* this.logRequest(method, url, data); */
+
+              const params = Object.keys(data).map((key) => {
+                return `${encodeURIComponent(key) }=${ encodeURIComponent(data[key])}`;
+              }).join('&');
+
+              if(method === 'GET') {
+                return fetch(url, {
+                  method: method,
+                  credentials: 'include'
+                }).then(result => result.json().then(json => {
+                  if(result.ok) return json.data;
+                  throw json.Message;
+                }));
+              }
+
+              return fetch(url, {
+                method: method,
+                body: params,
+                headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+                },
+                credentials: 'include'
+              }).then(result => {
+                if(result.ok) {
+                  return result.text().then(text => text && JSON.parse(text));
+                } else {
+                  return result.json().then(json => {
+                    throw json.Message;
+                  });
+                }
+              });
+            },
         }//end methods
     }
 </script>
